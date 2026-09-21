6dfb47d09dc81ad30f0fccc457931442049a69fd
Zafar <zavlatkamalov@gmail.com>
2026-09-21 11:14:33 +0500

    ы

--- Операторы цикла/Цикл с предусловием (while)/I.py
+++ Операторы цикла/Цикл с предусловием (while)/I.py
@@ -3,4 +3,4 @@ nums = list(map(int, input().split()))
 odd_min = min(nums[0::2]) if nums else 0
 even_max = max(nums[1::2]) if len(nums) > 1 else 0
 
-print(odd_min + even_max)
+print(even_max + odd_min)
